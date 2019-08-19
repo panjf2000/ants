d1b4e8b1632fef4794dffcf64a1517bbcc523234
Andy Pan <panjf2000@gmail.com>
2019-08-19 16:35:58 +0800

    🍻Refine the `Pool.Release()` Method

--- pool.go
+++ pool.go
@@ -200,7 +200,7 @@ func (p *Pool) Tune(size int) {
 }
 
 // Release Closes this pool.
-func (p *Pool) Release() error {
+func (p *Pool) Release() {
 	p.once.Do(func() {
 		atomic.StoreInt32(&p.release, 1)
 		p.lock.Lock()
@@ -212,7 +212,6 @@ func (p *Pool) Release() error {
 		p.workers = nil
 		p.lock.Unlock()
 	})
-	return nil
 }
 
 //---------------------------------------------------------------------------
--- pool_func.go
+++ pool_func.go
@@ -205,7 +205,7 @@ func (p *PoolWithFunc) Tune(size int) {
 }
 
 // Release Closed this pool.
-func (p *PoolWithFunc) Release() error {
+func (p *PoolWithFunc) Release() {
 	p.once.Do(func() {
 		atomic.StoreInt32(&p.release, 1)
 		p.lock.Lock()
@@ -217,7 +217,6 @@ func (p *PoolWithFunc) Release() error {
 		p.workers = nil
 		p.lock.Unlock()
 	})
-	return nil
 }
 
 //---------------------------------------------------------------------------
